bff7b9ef320c40832b4292d9eaf40eb470415e7b
Andrew Jung <andrewjung@andrews-imac.lan>
2022-09-13 21:02:22 -0400

    Setup: Initial boiler plate

---
+++ src/client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Turn Autosave On</h1>
+    </div>
+  );
+}
+
+render(<App />, document.querySelector('#root'));
