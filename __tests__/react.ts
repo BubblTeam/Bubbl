e120241d4a6161adc7dadc827fbde31f78d0eb8b
Kevin Le <kevinle1003@gmail.com>
2022-09-15 10:23:59 -0500

    unit test setup

---
+++ __tests__/react.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import MenuContainer from '../src/client/Containers/MenuContainer'
+
+// describe('Unit testing React components', () => {
+//   describe('MenuContainer', () => {
+//     const props = {
+//       statusMessage: 'What should Squirtle do?'
+//     };
+
+//     test('Renders the passed-in text with the label in bold', () => {
+//       render(<MenuContainer statusMessage={props.statusMessage} />);
+//       // expect(screen.getByText('Market ID:').nextSibling).toHaveTextContent(props.index.toString());
+//     });
+//   });
+// });
+
+describe('Menu Container tests', () => {
+  
+
+
+})
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/client/app.tsx',
+  entry: './src/client/App.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
